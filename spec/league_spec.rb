cfa69abb8a8064cf11881baa0f8df72575b9175f
MarkY <markcyen@yahoo.com>
2021-04-07 10:04:41 -0600

    Create league tests

---
+++ spec/league_spec.rb
@@ -0,0 +1,73 @@
+require 'rspec'
+require './lib/league'
+require './lib/team'
+require './lib/player'
+
+describe '#initialize' do
+  it 'exists' do
+    premier = League.new("Premier League")
+    expect(premier).to be_instance_of(League)
+  end
+
+  it 'has name and check for teams as Array' do
+    premier = League.new("Premier League")
+    expect(premier).to eq("Premier League")
+    expect(premier).to be(Array)
+  end
+end
+
+describe 'teams in premier league' do
+  it 'check for teams in premier leage' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_players(roy)
+    richmond.add_players(sam)
+    jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
+    fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
+    manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
+    manchester.add_players(jamie)
+    manchester.add_players(fernandinho)
+    premier.add_team(richmond)
+    premier.add_team(manchester)
+
+    expected = [richmond, manchester]
+    expect(premier.teams).to eq(expected)
+  end
+
+  it 'check for captains' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_players(roy)
+    richmond.add_players(sam)
+    jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
+    fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
+    manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
+    manchester.add_players(jamie)
+    manchester.add_players(fernandinho)
+    premier.add_team(richmond)
+    premier.add_team(manchester)
+
+    expected = [richmond.captain, manchester.captain]
+    expect(premier.captains).to eq(expected)
+  end
+
+  it 'check for players by each team' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_players(roy)
+    richmond.add_players(sam)
+    jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
+    fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
+    manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
+    manchester.add_players(jamie)
+    manchester.add_players(fernandinho)
+    premier.add_team(richmond)
+    premier.add_team(manchester)
+
+    expected = []
+    expect(premier.players_by_team).to eq(expected)
+  end
+end
