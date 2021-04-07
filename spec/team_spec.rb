a40e02700b1830e5a0c513db2db6b9e441984918
MarkY <markcyen@yahoo.com>
2021-04-07 09:50:03 -0600

    Refactor team tests

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,15 +3,42 @@ require './lib/player'
 require './lib/team'
 
 describe Team do
-  it 'check for data in teams' do
+  it 'check for team name and coach' do
     roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
     sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
     richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
 
-    expect(richmond.name).to eq('AFC Richmond')
+    expect(richmond.name).to eq("AFC Richmond")
     expect(richmond.coach).to eq("Ted Lasso")
+  end
+
+  it 'check for players in team' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_players(roy)
+    richmond.add_players(sam)
+
     expect(richmond.players).to eq([roy, sam])
+  end
+
+  it 'check for total salary of players in team' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_players(roy)
+    richmond.add_players(sam)
+
     expect(richmond.total_salary).to eq(1600000)
+  end
+
+  it 'check for captain on team' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_players(roy)
+    richmond.add_players(sam)
+
     expect(richmond.captain).to eq("Roy Kent")
   end
 
@@ -19,9 +46,10 @@ describe Team do
     roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
     sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
     richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    richmond.add_players(roy)
+    richmond.add_players(sam)
 
     expect(richmond.positions_filled).to eq([roy.position, sam.position])
   end
-
 
 end
